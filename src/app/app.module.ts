2512b4a6926ff03962835d0547f2c16caff92a59
porwalarchit <architporwal@gmail.com>
2023-04-12 16:57:42 +0530

    Created 2 containers header and nav using ng generate component <name>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ContainerComponent } from './Container/container.component';
+import { NavComponent } from './nav/nav.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContainerComponent
+    ContainerComponent,
+    NavComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
